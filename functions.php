0c8b426f5f5b1d5f32b5c24d76997af3c4a82ff2
ARCHTKT <antonio.sanchez.arq@gmail.com>
2018-01-11 13:49:54 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Zero to Null Genesis Child Theme.
+ *
+ * @package      Zero-to-Null
+ * @link         https://demo.archtkt.com/zero/
+ * @author       ARCHTKT
+ * @copyright    Copyright © 2018 ARCHTKT
+ * @license      GPL-2.0+
+ */
+ 
+ // If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Define theme constants.
+define( 'CHILD_THEME_NAME', 'Zero to Null' );
+define( 'CHILD_THEME_URL', 'https://seothemes.com/themes/genesis-starter' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
